5e0446c9b649f381507847c598f70bfb83a5d655
Alexander Polok <ipoloka@fit.vut.cz>
2026-02-20 01:05:14 +0100

    NOTSOFAR simulation recipe

--- simulate_conversations.sh
+++ simulate_conversations.sh
@@ -7,7 +7,7 @@ CONFIGS=("flat" "ami_estimate" "nsf_estimate" "nsf_estimate_boosted")
 
 for CONFIG in "${CONFIGS[@]}"; do
     echo "Processing config: $CONFIG"
-#    python "${FastMSS_ROOT}/sim_nsf_ihm.py" --config-name "$CONFIG"
+    python "${FastMSS_ROOT}/sim_nsf_ihm.py" --config-name "$CONFIG"
     python "${MADP_ROOT}/src/pre_segment_using_alignments.py" \
         --input "$SIMULATED_OUT/${CONFIG}/manifests/synth-notsofar1_ihm-train-cuts.jsonl.gz" \
         --output "$SIMULATED_OUT/${CONFIG}/manifests/synth-notsofar1_ihm-train-cuts_30s.jsonl.gz" \
